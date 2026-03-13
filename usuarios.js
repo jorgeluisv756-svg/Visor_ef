c3d96bc94150a3befb6bf2bda7c4f70a57a517a8
jorgeluisv756-svg <jorgeluisv756@gmail.com>
2026-03-13 15:26:08 -0600

    Add files via upload

---
+++ usuarios.js
@@ -0,0 +1,12 @@
+// Lista simulada de usuarios por área
+const usuariosPermitidos = [
+  { usuario: "admin", password: "1234", area: "Secretaría de Desarrollo Económico" },
+  { usuario: "energia", password: "1234", area: "Agencia Estatal de Energía" },
+  { usuario: "fomento", password: "1234", area: "Subsecretaría de Fomento Económico" },
+  { usuario: "plata", password: "1234", area: "Agencia de Desarrollo Valle de Plata" },
+  { usuario: "administración", password: "1234", area: "Coordinación de Administración y Planeación" },
+  { usuario: "cofoin", password: "1234", area: "Corporación de Fomento de Infraestructura Industrial" },
+  { usuario: "ihce", password: "1234", area: "Instituto Hidalguense de Competitividad Empresarial" },
+  { usuario: "inversiones", password: "1234", area: "Subsecretaría de Inversiones y Economía Sectorial" }
+  
+];
