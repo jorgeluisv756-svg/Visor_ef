9809c7e428af678bb71389e345eeb072eff54bf1
jorgeluisv756-svg <jorgeluisv756@gmail.com>
2026-03-13 16:05:13 -0600

    Add files via upload

--- usuarios.js
+++ usuarios.js
@@ -3,7 +3,7 @@ const usuariosPermitidos = [
   { usuario: "admin", password: "1234", area: "Secretaría de Desarrollo Económico" },
   { usuario: "energia", password: "1234", area: "Agencia Estatal de Energía" },
   { usuario: "fomento", password: "1234", area: "Subsecretaría de Fomento Económico" },
-  { usuario: "plata", password: "1234", area: "Agencia de Desarrollo Valle de Plata" },
+  { usuario: "platah", password: "1234", area: "Agencia de Desarrollo Valle de Plata" },
   { usuario: "administración", password: "1234", area: "Coordinación de Administración y Planeación" },
   { usuario: "cofoin", password: "1234", area: "Corporación de Fomento de Infraestructura Industrial" },
   { usuario: "ihce", password: "1234", area: "Instituto Hidalguense de Competitividad Empresarial" },
